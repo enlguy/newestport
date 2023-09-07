a4e3bb69534fa30548e4a22e0d97b65dc30a9a22
Jack <jack282323@gmail.com>
2023-09-07 09:39:42 +0200

    Changing image file size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex items-start gap-5`}
       >
         <img
           className="h-auto max-w-lg ml-5 rounded-lg"
